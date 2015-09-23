dc7a2388123c088f3aaf96bae0144e0d93b0d36a
Marcus Hitchins <marcus7777@gmail.com>
2015-09-23 11:42:54 +0100

    go dev fix

--- go-dev.sh
+++ go-dev.sh
@@ -1,1 +1,1 @@
-gulp && rsync -a ./dist/ root@dev.essentialhotels.co.uk:/var/www/sites/dev2.essentialhotels.co.uk/ --exclude index.json --exclude .htaccess
+bower update && gulp && rsync -a ./dist/ root@dev.essentialhotels.co.uk:/var/www/sites/dev2.essentialhotels.co.uk/ --exclude index.json --exclude .htaccess
